2257bd021b9d0a109b6c243442a56024593c8ff2
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-17 16:31:07 -0500

    Added styling for homepage

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
-
 // Index route
 app.get("/", function (req, res) {
   res.render("index", {title: "Home"})
