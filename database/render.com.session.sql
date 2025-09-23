34813991a3643c36b0d5f659a45d13bf7e533453
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-22 21:00:48 -0500

    updated my database recreation file to include the 4th objective

--- database/render.com.session.sql
+++ database/render.com.session.sql
@@ -241,6 +241,11 @@ SET
 inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
 inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
 
+-- Update all 'GM' inv_description to replace 'the small interiors' with 'a huge interior'
+UPDATE "inventory"
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_make = 'GM';
+
 
 -- Update all records in the inventory table to add "/vehicles" to the middle of the file path in the inv_image and inv_thumbnail columns using a single query
 UPDATE "inventory"
