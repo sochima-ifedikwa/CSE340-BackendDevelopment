e79b2f7a6edacb3dab906f0683b717d067986d4b
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-22 21:07:48 -0500

    Remove redundant update for image paths in inventory table

--- database/render.com.session.sql
+++ database/render.com.session.sql
@@ -236,11 +236,6 @@ VALUES   (
   );
 
 
-UPDATE "inventory"
-SET 
-inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
-inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
-
 -- Update all 'GM' inv_description to replace 'the small interiors' with 'a huge interior'
 UPDATE "inventory"
 SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
