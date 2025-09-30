d0a87d837a519436733be020e2043dcd0a97d254
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-30 04:38:09 -0500

    Refactor buildByClassificationId to include error handling and improve response for empty classifications

--- controllers/invController.js
+++ controllers/invController.js
@@ -7,16 +7,23 @@ const invCont = {}
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
+  try {
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    if (!data || !data.length) {
+      return res.status(404).send('No vehicles found for this classification')
+    }
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = invCont
