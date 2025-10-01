75318561bc125889903e298736058aa89274a5aa
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-30 22:10:12 -0500

    Update database connection string and clean up SQL scripts; refactor inventory model to return data rows

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities")
+// const invChecks = require("../utilities/inventory-validation")
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
